bbb1244b6b6d540ef90378342b7a1777e3c3b6e2
alfonsoruelas <alfonsoruelas@gmail.com>
2021-03-18 18:00:26 -0600

    inicial

---
+++ bebe.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="estilos.css">  
+    <title>Bienvenido Bebé</title>
+</head>
+<body>
+    <div class="contenedor">
+        <?php require 'bebe.html';?>
+        <?php require 'bebe.html';?>
+        <?php require 'bebe.html';?>
+        <?php require 'bebe.html';?>
+    </div>
+    
+    <script src="https://unpkg.com/ionicons@5.2.3/dist/ionicons.js"></script>
+</body>
+</html>
---
+++ historia.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="estilos.css">  
+    <title>Historia</title>
+</head>
+<body>
+    <div class="contenedor">
+        <?php require 'historia.html';?>
+        <?php require 'historia.html';?>
+        <?php require 'historia.html';?>
+        <?php require 'historia.html';?>
+    </div>
+    
+</body>
+</html>
